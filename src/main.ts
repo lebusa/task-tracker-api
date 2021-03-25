6a7b446d608466412a0bcf57c80c91633a002221
Motebang Lebusa <teby4real@yahoo.com>
2021-03-25 22:36:17 +0200

    enabled cors; changed db 2 tasks;

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,16 @@ import { AppModule } from './app.module';
 const PORT = 4000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {logger: ['log', 'error', 'warn', 'debug', 'verbose'],});
+
+  app.enableCors();
+
   app.setGlobalPrefix('/api');
-  await app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+
+  await app.listen(
+    PORT, 
+    () => {
+      console.log(`Server running on port ${PORT}`);
+    });
 }
 bootstrap();
