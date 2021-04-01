c5e7402bf9e6cbecfc516fb2ea0ef09e2f8e623c
Motebang Lebusa <teby4real@yahoo.com>
2021-04-01 17:28:26 +0200

    modified the host in the database

--- src/database/db.provider.ts
+++ src/database/db.provider.ts
@@ -1,9 +1,11 @@
 import * as mongoose from 'mongoose';
 
+const HOST = 'localhost'; // change to this to an appropriate host
+
 export const databaseProviders = [
     {
         provide: 'DATABASE_CONNECTION',
         useFactory: async (): Promise<typeof mongoose> =>
-        await mongoose.connect('mongodb://localhost/tasks', { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }),
+        await mongoose.connect(`mongodb://${HOST}/tasks`, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }),
     },
 ];
